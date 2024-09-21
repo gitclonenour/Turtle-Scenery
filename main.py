dc84a6d811f9090e9830b64875b61ec3c7e7a074
nour <noursstudies@gmail.com>
2024-09-21 23:30:48 +0400

    Refactored table.py

--- main.py
+++ main.py
@@ -1,19 +1,117 @@
-import turtle as t
+import turtle as t 
 
-def table():
-    pass #Delete this line when you write your code
+def tweak(): #Setting up turtle for the drawing
+    t.pencolor("black")
+    t.pensize(3)
+    t.speed(10)
 
-def cake():
-    pass #Delete this line when you write your code
+def plate(radius): #Drawing the plate for the cake
+    t.pendown()
+    t.fillcolor("gray90")
+    t.pencolor("gray90")
+    t.begin_fill()
+    t.forward(radius*1.5) #Bottom side first half
+    t.left(90)
+    t.forward(radius*0.2) #Right side
+    t.left(90)
+    t.forward(radius*3) #Top side
+    t.left(90)
+    t.forward(radius*0.2) #Left side
+    t.left(90)
+    t.forward(radius*1.5) #Bottom side second half
+    t.end_fill()
+    t.penup()
 
-def cake_decorations():
-    pass #Delete this line when you write your code
+def prepare_next_layer(distance): #Moving the turtle to the next layer of the cake
+    t.setheading(90) #Setting the turtle to face up
+    t.forward(distance) 
 
-def background():
-    pass #Delete this line when you write your code
+def cake_layer(color, width, height): #Drawing the cake layers with the given color, width, and height
+    t.setheading(0)
+    t.pendown()
+    t.fillcolor(color)
+    t.pencolor(color)
+    t.begin_fill()
+    t.forward(width) #Bottom side first half
+    t.left(90)
+    t.forward(height) #Right side
+    t.left(90)
+    t.forward(width*2) #Top side
+    t.left(90)
+    t.forward(height) #Left side
+    t.left(90)
+    t.forward(width) #Bottom side second half
+    t.end_fill()
+    t.penup()
+
+def cake(radius): #Drawing the cake with the given radius
+    #Setting up the height of the layers using the radius
+    layer1_height = radius*0.6 
+    layer2_height = radius*0.3
+    layer3_height = radius*0.15
+    #Drawing the layers
+    cake_layer("DeepPink2", radius, layer1_height) #Bottom layer
+    prepare_next_layer(layer1_height)
+    cake_layer("gray20", radius, layer2_height) #Middle layer 1
+    prepare_next_layer(layer2_height)
+    cake_layer("chocolate", radius, layer3_height) #Middle layer 2
+    prepare_next_layer(layer3_height)
+    cake_layer("bisque", radius, layer1_height) #Top of the cake
+
+def draw_table(color):
+    #table top
+    t.fillcolor(color)
+    t.begin_fill()
+    t.penup()
+    t.goto(-100, 50)  
+    t.pendown()
+    t.forward(200)    
+    t.right(90)
+    t.forward(20)    
+    t.right(90)
+    t.forward(200)   
+    t.right(90)
+    t.forward(20)     
+    t.right(90) 
+    t.end_fill()
+    t.penup()
+    t.goto(-90, 30)
+    
+    #legs
+    t.up()
+    t.right(90)
+    t.down()
+    t.forward(80)
+    t.left(90)
+    t.up()
+    t.forward(40)
+    t.left(90)
+    t.forward(20)
+    t.down()
+    t.forward(60)
+    t.up()
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+    t.down()
+    t.forward(60)
+    t.up()
+    t.forward(20)
+    t.left(90)
+    t.forward(40)
+    t.down()
+    t.left(90)
+    t.forward(80)
 
 def main():
-    pass
+    table_color = input("Enter the color of the table: ") #Asking the user for the color of the table
+    length = int(input("Enter the radius of the cake: ")) #Asking the user for the radius of the cake
+    tweak()
+    draw_table(table_color)
+    plate(length)
+    prepare_next_layer(length*0.2) #Moving the turtle to the first layer of the cake
+    cake(length)
+    t.exitonclick()
 
 if __name__ == "__main__":
     main()
