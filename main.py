aee72c005c426446cbef44ff5359893bc5be76b3
nour <noursstudies@gmail.com>
2024-09-22 22:00:18 +0400

    Polished code.

--- main.py
+++ main.py
@@ -5,20 +5,27 @@ The cake will also have decorations on top.
 The table will have four legs and a top with the given color, length, and height.
 Project by: Noureldin, Saloni, and Mehdi
 """
-import turtle as t 
+import turtle as t
+
+#Setting up the directions for the setheading() function
+east = 0
+north = 90
+west = 180
+south = 270
+window_width = 400
+window_height = 300
 
 def tweak(): #Setting up turtle for the drawing
     t.pencolor("black")
     t.pensize(2)
-    t.speed(6)
+    t.speed(7)
 
 #Noureldin
 def plate(radius): #Drawing the plate for the cake
     t.goto(0,0)
-    t.setheading(0)
+    t.setheading(east)
     t.pendown()
     t.fillcolor("gray60")
-    t.pencolor("gray60")
     t.begin_fill()
     t.forward(radius*1.5) #Bottom side first half
     t.left(90)
@@ -33,14 +40,13 @@ def plate(radius): #Drawing the plate for the cake
     t.penup()
 
 def prepare_next_layer(distance): #Moving the turtle to the next layer of the cake
-    t.setheading(90) #Setting the turtle to face up
+    t.setheading(north) #Setting the turtle to face up
     t.forward(distance) 
 
 def cake_layer(color, width, height): #Drawing the cake layers with the given color, width, and height
-    t.setheading(0)
+    t.setheading(east)
     t.pendown()
     t.fillcolor(color)
-    t.pencolor(color)
     t.begin_fill()
     t.forward(width) #Bottom side first half
     t.left(90)
@@ -99,67 +105,78 @@ def draw_table(length, height, color): #Drawing the table with the given length,
     reset_turtle(180, 0.9)
     #First leg
     t.pendown()
-    t.setheading(270)
+    t.setheading(south)
     t.forward(100)
     t.penup()
 
     reset_turtle(180, 0.6)
     #Second leg
     t.pendown()
-    t.setheading(270)
+    t.setheading(south)
     t.forward(80)
     t.penup()
 
     reset_turtle(0, 0.9)
     #Third leg
     t.pendown()
-    t.setheading(270)
+    t.setheading(south)
     t.forward(100)
     t.penup()
 
     reset_turtle(0, 0.6)
     #Fourth leg
     t.pendown()
-    t.setheading(270)
+    t.setheading(south)
     t.forward(80)
     t.penup()
 
 #Mehdi
-def decorations(): #Drawing decorations on the cake
-    pass #Remove this line when you start working on this function
-
+def decorations(width): #Drawing decorations on the cake
+    cake_layer("white", width+10, 20) #Drawing a red square on top of the cake
 
 def main():
-    t.bgcolor("LightBlue1")
+
+    t.screensize(canvwidth=window_width, canvheight=window_height, bg="LightBlue1")  #Setting up the screen
+
+    """
     table_color = input("Enter the color of the table: ") #Asking the user for the color of the table
     print("\nWe recommend a table length of at least 200.")
     table_length = int(input("Enter the length of the table: ")) #Asking the user for the length of the table
     print("\nWe recommend a table height of 40.")
     table_height = int(input("Enter the height of the table: ")) #Asking the user for the height of the table
     cake_radius = int(input("Enter the radius of the cake: ")) #Asking the user for the radius of the cake
-
-    #Debugging
-    """    
-    table_color = "brown"
-    table_length = 200
-    table_height = 40
-    cake_radius = 100
     """
-
+    #Debugging
+    
+    table_color = "brown"
+    table_length = 401
+    table_height = 40
+    cake_radius = 75
+    
     #Validity checks by Noureldin.
-    if cake_radius > table_length/2:
+    #Limitation: The program only does this validity check once. If the user enters an invalid value again, the program will proceed.
+    if table_length > window_width:
+        print("The table is too big for the screen!")
+        table_length = int(input(f"Enter a value smaller than {window_width} for the table: ")) 
+    elif table_height > window_height:
+        print("The table is too tall for the screen!")
+        table_height = int(input(f"Enter a value smaller than {window_height} for the table: "))
+    elif cake_radius > window_width:
+        print("The cake is too big for the screen!")
+        cake_radius = int(input(f"Enter a value smaller than {table_length/2} for your cake: "))
+    elif cake_radius > table_length/2:
         print("The cake is too big for the table!")
         cake_radius = int(input(f"Enter a value smaller than {table_length/2} for your cake: "))
     else:
         print("The cake fits on the table! Enjoy your cake!")
 
-    tweak()
+    tweak() #Setting up the turtle for drawing
     draw_table(table_length, table_height, table_color) #Drawing the table
     plate(cake_radius) #Drawing the plate for the cake
-    t.goto(0,0)
-    t.setheading(0)
+    t.goto(0,0) #Moving the turtle to the center of the plate
     prepare_next_layer(cake_radius*0.2) #Moving the turtle to the first layer of the cake
     cake(cake_radius) #Drawing the cake
+    decorations(cake_radius) #Drawing decorations on the cake
     t.exitonclick()
 
 if __name__ == "__main__":
